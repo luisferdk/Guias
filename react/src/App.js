40606b47389a46960d38a2d38845d352b6b9a413
luisferdk <luisdk.03@gmail.com>
2020-03-04 11:12:09 -0400

    Router

--- react/src/App.js
+++ react/src/App.js
@@ -6,6 +6,7 @@ import Presentational from './components/Presentational';
 import Stateless from './components/Stateless';
 import Stateful from './components/Stateful';
 import StyledComponent from './components/StyledComponent';
+import Router from './components/Router';
 
 /* Component with Styled Component */
 
@@ -15,6 +16,8 @@ const App = () => (
     <Stateless></Stateless>
     <Stateful></Stateful>
     <StyledComponent></StyledComponent>
+    <h1>Router</h1>
+    <Router></Router>
   </div>
 )
 export default App;
